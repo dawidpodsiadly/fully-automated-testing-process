5014dfe8c7701060b736884a4de4c025feb0bfdd
dpodsiadly <d.podsiadly@avsystem.com>
2026-04-26 08:39:40 +0200

    Add metrics and Prometheus

--- app/api/src/server.js
+++ app/api/src/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const passport = require('passport');
 const morgan = require('morgan');
+const client = require('prom-client');
 const userRoutes = require('./routes/userRoutes');
 const auth = require('./auth');
 const { createTestUsers } = require('./utils/testUsersAutoSetup');
@@ -17,10 +18,30 @@ const corsOptions = {
   credentials: true,
 };
 
+const apiRequestsTotal = new client.Counter({
+  name: 'api_requests_total',
+  help: 'Total number of HTTP requests handled by the API',
+  labelNames: ['method', 'status_code'],
+});
+
 app.use(cors(corsOptions));
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(passport.initialize());
+app.use((req, res, next) => {
+  if (req.path === '/metrics') {
+    return next();
+  }
+
+  res.on('finish', () => {
+    apiRequestsTotal.inc({
+      method: req.method,
+      status_code: String(res.statusCode),
+    });
+  });
+
+  next();
+});
 
 const mongoUri =
   process.env.MONGO_URI ||
@@ -34,6 +55,11 @@ mongoose
     await createTestUsers();
   })
   .catch(err => console.log(err));
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 app.use(userRoutes);
 
